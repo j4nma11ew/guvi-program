c7e4b17bb40ae4b228c38d56eec759e307852fc6
prakasha jency <jencymattew@gmail.com>
2018-12-02 23:49:15 +0530

    program to find vowel or consonant

---
+++ vowelconsonant.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf(" %c", &ch);
+     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
+      if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')  {
+		printf("Vowel");
+	}
+    else 
+    	printf("Consonant");
+	
+}
+else
+      	printf("Invalid");
+
+	 return 0;
+}   
